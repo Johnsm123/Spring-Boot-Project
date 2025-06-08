4545567bf93de734618f91a605c427f8e4b0a73f
Johnsm123 <ajohnsm2020@gmail.com>
2025-06-08 22:00:49 +0530

    DB commit

---
+++ Spring-Boot-DBUpdate/sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.Payload.CategoryDTO;
+import com.ecommerce.project.Payload.CategoryResponse;
+
+public interface CategoryService {
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+
+    CategoryDTO deleteCategory(Long categoryId);
+
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
+}
